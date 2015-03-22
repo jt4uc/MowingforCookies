1ac90659b8f31435e9985a49cf49e6348fb61aab
ayz7bs <ayz7bs@virginia.edu>
2015-03-22 01:11:26 -0400

    added most fields/methods for obstacle and spot

--- MowingforCookies/Spot.cs
+++ MowingforCookies/Spot.cs
@@ -5,8 +5,49 @@ using System.Text;
 
 namespace MowingforCookies
 {
-    class Spot
+    class Spot : Sprite
     {
-        
+        public int x;
+        public int y;
+        public bool isTraversed;
+        public int travelCost; //of cookies
+        public int cookiesGained;
+        public Obstacle ob;
+        //tileTexture: Texture2D
+
+        public Spot(int x, int y, bool isTraversed, int travelCost, int cookiesGained
+            , Obstacle ob)
+        {
+            this.x = x;
+            this.y = y;
+            this.isTraversed = isTraversed;
+            this.travelCost = travelCost;
+            this.cookiesGained = cookiesGained;
+            this.ob = ob;
+        }
+
+        //Initialized
+
+        public bool mowerHasTraversed(){
+            return this.isTraversed;
+        }
+
+        //update
+
+        //draw
+
+        //traversedEffect
+
+        public Obstacle getObstacle()
+        {
+            return ob;
+        }
+
+        public void setObstacle(Obstacle ob)
+        {
+            this.ob = ob;
+        }
+
+
     }
 }

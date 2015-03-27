44644f91f6f9cdf4a3f17cd878e2061a96da81bc
jt4uc <jt4uc@virginia.edu>
2015-03-26 20:15:21 -0400

    Added enemy

---
+++ MowingforCookies/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+
+namespace MowingforCookies
+{
+    class Enemy : Sprite
+    {
+        public int x;
+        public int y;
+        public int moveIndex;   
+        public Spot currentLocation;
+        public Spot targetLocation;
+        public bool alive;
+        public double speed;
+        public Texture2D enemyTexture;
+        //private Rectangle collisionBox;
+        //public Animated Sprite?? mowerTextureMap
+
+        //Content Manager?
+        public Enemy(Spot currentLocation, int cookies)
+        {
+
+            this.currentLocation = currentLocation;
+            this.moveIndex = 0;
+            this.x = currentLocation.x + 25;
+            this.y = currentLocation.y + 25;
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            image = content.Load<Texture2D>("gnome.gif");
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(image, new Rectangle(x, y, 48, 50), Color.White);
+        }
+
+        public void Update(Controls controls, List<Spot> patches, GameTime gameTime)
+        {
+            Move(mower, patches);
+        }
+
+        public void Move(Mower mower, List<Spot> patches)
+        {
+            // for the beta
+        }
+
+    }
+}
